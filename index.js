6956ceed5cf740518e7e2e852decaaf4d42ce451
Henry <h@huawei.com>
2021-07-29 14:27:36 +0800

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+import { MathLib } from '@wanwei/math'
+//import { Usual } from '@wanwei/modeldef'
+
+//const fs = require('fs')
+//const math = require('@wanwei/math')
+
+
+const data = fs.readFileSync('m.json', { encoding: 'utf-8', flag: 'r' })
+
+//const
+
+console.log(data)
